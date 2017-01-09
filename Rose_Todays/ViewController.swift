b88cc68be7a5b1da5cdf722a3dfbbc61ffb27e77
Tyler Rose <tylerjosephrose@gmail.com>
2017-01-09 12:47:01 -0500

    Display a single quote from the list

--- Rose_Todays/ViewController.swift
+++ Rose_Todays/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	// declare our array of quotes
+	//var quotes : Array<String>!
+	var quotes : NSArray!
 
+	@IBOutlet weak var lblQuote: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		// 1. Load contents of ListOfQuotes.plist to array
+		// create reference to app main bundle
+		let appBundle : Bundle = Bundle.main
+		// Get reference to file path
+		let filePath = appBundle.path(forResource: "ListOfQuotes", ofType: "plist")!
+		// Load quotes from filePath to quotes
+		quotes = NSArray(contentsOfFile: filePath)
+		// Get single quote from list
+		let singleQuote : String = quotes.object(at: 1) as! String
+		// Set label text to this singleQuote
+		lblQuote.text = singleQuote
 	}
 
 	override func didReceiveMemoryWarning() {
