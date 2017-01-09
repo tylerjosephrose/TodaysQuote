3bdb8752267425dde99b7778431c1772e5b30555
Tyler Rose <tylerjosephrose@gmail.com>
2017-01-09 10:05:02 -0500

    Initial Commit

---
+++ Rose_Todays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rose_Todays
+//
+//  Created by Tyler Rose on 1/9/17.
+//  Copyright © 2017 Tyler Rose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
